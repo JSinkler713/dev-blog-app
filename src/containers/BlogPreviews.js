9a9c6cdc15639e7368874f47cedc587bda187c33
James Sinkler <jamessinkler713@gmail.com>
2020-01-02 08:02:12 -0800

    adds second blog article

--- src/containers/BlogPreviews.js
+++ src/containers/BlogPreviews.js
@@ -26,6 +26,19 @@ Thanks for reading, check out the React site for an intro and get started with t
             </p>
           </div>
         </article>
+        <article>
+          <h2>Code Pen</h2>
+          <p>By J.Sinkler</p>
+          <div>
+            <p>
+            CodePen is a sandbox of sorts where you can test ideas, or fully develop a website. The benefit is in the layout where you can see html, css, and javascript side by side, while also looking at a preview of what your site would look like. I liked being able to make quick changes and see ti immediately while staying on the same page where I could see all my code.
+            </p>
+            <p>
+            There are also a lot of things to learn on CodePen. People make all sorts of small components that you might want to borrow ideas from. I've seen an svg tool to help you build non-rectangular divs, to a cat that chases it's tail to represent a page-loading. There are tons of examples that people share, many of which the authors spend a good deal of time on.</p>
+            <p>
+            You should definately take a look at the examples people put up on CodePen to be inspired. I recently used it to practice some grid-layout with css. My webpage is built with React but instead of putting all that js in. I just used static containers, but could design with css. I then just copied the css code into a css document for that pages styling. Remember to change 'class' to 'className' and your set. Check it out, and share your success in the comments below.</p>
+          </div>
+        </article>
       </div>
     )
   }
